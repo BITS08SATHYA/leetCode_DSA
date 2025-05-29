18440acb10cb86156db6021e9e7e2d4ec110f660
Sathya <neelasathya06@gmail.com>
2025-05-29 08:34:59 +0530

    01-kth-symbol-recursion understood and implemented

---
+++ DP/knapsack/04_Space_Optimized_tabulation.py
@@ -0,0 +1,15 @@
+def knapsack(W, wt, val, n):
+
+    prev = [0]*(W+1)
+    curr = [0] * (W + 1)
+
+    for i in range(1, n+1):
+        for j in range(1, W + 1):
+            exclude = prev[j]
+            include = 0
+            if wt[i-1] <= j:
+                include = val[i-1] + prev[j-wt[i-1]]
+            curr[j] = max(exclude, include)
+        prev = curr[:]
+    return curr[W]
+
