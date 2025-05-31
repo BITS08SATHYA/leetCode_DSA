7d15f350571dcd28bb107963201c7f5793948e74
Sathya <neelasathya06@gmail.com>
2025-05-31 08:43:40 +0530

    premute done

---
+++ recursion/permute/permute.py
@@ -0,0 +1,16 @@
+def permute(nums):
+    n = len(nums)
+    res = []
+    def helper(index):
+        # base case
+        if index == n - 1:
+            res.append(nums[:])
+            return
+        for j in range(index, n):
+            nums[index], nums[j] = nums[j],nums[index]
+            helper(index + 1)
+            nums[index], nums[j] = nums[j],nums[index]
+    helper(0)
+    return res, len(res)
+
+print(permute([1,2,3]))
